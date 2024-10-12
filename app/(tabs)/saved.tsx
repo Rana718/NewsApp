034975922e4288b0458e0f567a594f6a4427ba6b
Rana718 <ranadolui718@gmail.com>
2024-10-12 18:45:06 +0530

    approx complete

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,72 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { Link, Stack } from "expo-router";
+import Loading from "@/components/Loading";
+import Animated from "react-native-reanimated";
+import NewsItem from "@/components/NewsItem";
+import { useIsFocused } from "@react-navigation/native";
 
 type Props = {};
 
 export default function Saved({}: Props) {
+    const [bookmarkNews, setBookmarkedNews] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const isFocused = useIsFocused();
+
+    useEffect(()=>{
+        fetchBookmark();
+    },[isFocused])
+
+    const fetchBookmark = async () => {
+        try {
+            setIsLoading(true);
+            const token = await AsyncStorage.getItem("bookmark");
+            const res = token ? JSON.parse(token) : null;
+            
+            if (res) {
+                let query_string = res.join(',');
+                const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${query_string}`;
+                const response = await axios.get(URL);
+                setBookmarkedNews(response.data.results);
+            } else {
+                setBookmarkedNews([]);
+            }
+        } catch (error) {
+            console.error("Error fetching bookmarked news:", error);
+            setBookmarkedNews([]);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    
+
     return(
-        <View className="flex-1 items-center justify-center">
-            <Text>Saved Screen</Text>
-        </View>
+        <>
+            <Stack.Screen options={{
+                headerShown: true,
+            }}/>
+
+            <View className="flex-1 m-5">
+                {isLoading ? (
+                    <Loading size={"large"}/>
+                ): (
+                    <Animated.FlatList
+                        data={bookmarkNews}
+                        keyExtractor={(_, index) => `list_items${index}`}
+                        renderItem={({ item }) => (
+                            //@ts-expect-error
+                            <Link href={`/newsde/${item.article_id}`} asChild>
+                                <TouchableOpacity>
+                                    <NewsItem item={item} />
+                                </TouchableOpacity>
+                            </Link>
+                        )}
+                        showsVerticalScrollIndicator={false}
+                    />
+                )}
+            </View>
+        </>
     )
 }
--- app/newsde/[id].tsx
+++ app/newsde/[id].tsx
@@ -8,141 +8,140 @@ import Loading from "@/components/Loading";
 import moment from "moment";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
 type Params = {
-    id?: string; 
-}
+  id?: string;
+};
 
 type Props = {};
 
 export default function NewsDetails({}: Props) {
-    const { id } = useLocalSearchParams<Params>();
-    const [news, setNews] = useState<NewsDataType[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [bookmarked, setBookmarked] = useState(false);
+  const { id } = useLocalSearchParams<Params>();
+  const [news, setNews] = useState<NewsDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [bookmarked, setBookmarked] = useState(false);
 
-    useEffect(()=>{
-        getNews();
-    },[]);
+  useEffect(() => {
+    getNews();
+  }, []);
 
-    useEffect(()=>{
-        if(!isLoading){
-            renderBookmark(news[0].article_id);
-        }
-    },[isLoading]);
-
-    const getNews = async () => {
-        try{
-            const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${id}`
-            const response = await axios .get(URL);
-
-            if(response && response.data){
-                setNews(response.data.results);
-                setIsLoading(false);
-            }
-
-        }catch(e: any){
-            console.log(e);
-        }
+  useEffect(() => {
+    if (!isLoading && news.length > 0) {
+      checkBookmark(news[0].article_id);
     }
+  }, [isLoading, news]);
 
-    const renderBookmark = async(newsId: string)=>{
-        await AsyncStorage.getItem("bookmark").then((token)=>{
-            const res = JSON.parse(token as string);
-            if(res != null){
-                let data = res.find((value: string) => value === newsId);
-                return data == null ? setBookmarked(false) : setBookmarked(true);
-            }
-        });
+  const getNews = async () => {
+    try {
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${id}`;
+      const response = await axios.get(URL);
+
+      if (response && response.data) {
+        setNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (e: any) {
+      console.log(e);
     }
+  };
 
-    const saveBookmark = async (newsId: string) => {
-        setBookmarked(true);
-        await AsyncStorage.getItem("bookmark").then((token)=>{
-            const res = JSON.parse(token as string);
-            if(res != null){
-                let data = res.find((value: string) => value === newsId);
-                if(data === null){
-                    res.push(newsId);
-                    AsyncStorage.setItem("bookmark", JSON.stringify(res));
-                    alert("News has been bookmarked");
-                }
-            }else{
-                let bookmark = [];
-                bookmark.push(newsId);
-                AsyncStorage.setItem("bookmark", JSON.stringify(bookmark));
-                alert("News has been bookmarked");
-            }
-        })
+  const checkBookmark = async (newsId: string) => {
+    const token = await AsyncStorage.getItem("bookmark");
+    if (token) {
+      const bookmarks = JSON.parse(token) as string[];
+      setBookmarked(bookmarks.includes(newsId));
     }
+  };
 
-    const removeBookmark = async (newsId: string) => {
-        setBookmarked(false);
-        const bookmark = await AsyncStorage.getItem("bookmark").then((token)=>{
-            const res = JSON.parse(token as string);
-            return res.filter((value: string) => value !== newsId);
-        });
-        await AsyncStorage.setItem("bookmark", JSON.stringify(bookmark));
-        alert("News has been removed from bookmark");
+  const saveBookmark = async (newsId: string) => {
+    try {
+      setBookmarked(true);
+      const token = await AsyncStorage.getItem("bookmark");
+      const bookmarks = token ? (JSON.parse(token) as string[]) : [];
+
+      if (!bookmarks.includes(newsId)) {
+        bookmarks.push(newsId);
+        await AsyncStorage.setItem("bookmark", JSON.stringify(bookmarks));
+        alert("News has been bookmarked");
+      }
+    } catch (e: any) {
+      console.log(e);
     }
+  };
 
-    return(
-        <>
-            <Stack.Screen
-                options={{
-                    headerLeft: ()=> (
-                        <TouchableOpacity onPress={()=> router.back()}>
-                            <Ionicons name="arrow-back" size={22}/>
-                        </TouchableOpacity>
-                    ),
-                    headerRight: ()=>(
-                        <TouchableOpacity 
-                            onPress={()=> bookmarked ? 
-                                removeBookmark(news[0].article_id) :
-                                saveBookmark(news[0].article_id)
-                            }
-                        >
-                            <Ionicons name={bookmarked ? "heart" : "heart-outline"} color={bookmarked ? "red" : "black"} size={22}/>
-                        </TouchableOpacity>
-                    ),
-                    title: ''
-                }}
-            />
-            {isLoading ? (
-                <Loading size={"large"}/>
-            ):(
-                <ScrollView className="flex-1 bg-white px-5 pt-5 pb-5" >
+  const removeBookmark = async (newsId: string) => {
+    try {
+      setBookmarked(false);
+      const token = await AsyncStorage.getItem("bookmark");
+      if (token) {
+        let bookmarks = JSON.parse(token) as string[];
+        bookmarks = bookmarks.filter((id) => id !== newsId);
+        await AsyncStorage.setItem("bookmark", JSON.stringify(bookmarks));
+        alert("News has been removed from bookmarks");
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
 
-                    <Text className="text-xl font-semibold text-gray-700 mb-2">
-                        {news[0].title}
-                    </Text>
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={22} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() =>
+                bookmarked
+                  ? removeBookmark(news[0].article_id)
+                  : saveBookmark(news[0].article_id)
+              }
+            >
+              <Ionicons
+                name={bookmarked ? "heart" : "heart-outline"}
+                color={bookmarked ? "red" : "black"}
+                size={22}
+              />
+            </TouchableOpacity>
+          ),
+          title: "",
+        }}
+      />
+      {isLoading ? (
+        <Loading size={"large"} />
+      ) : (
+        <ScrollView className="flex-1 bg-white px-5 pt-5 pb-5">
+          <Text className="text-xl font-semibold text-gray-700 mb-2">
+            {news[0].title}
+          </Text>
 
-                    <View className="flex-row justify-between mb-4">
-                        <Text className="text-xs text-darkGrey">
-                            {moment(news[0].pubDate).format("MMMM DD hh:mm a")}
-                        </Text>
+          <View className="flex-row justify-between mb-4">
+            <Text className="text-xs text-darkGrey">
+              {moment(news[0].pubDate).format("MMMM DD hh:mm a")}
+            </Text>
 
-                        <Text className="text-xs text-darkGrey">
-                            {news[0].source_name}
-                        </Text>
-                    </View>
+            <Text className="text-xs text-darkGrey">{news[0].source_name}</Text>
+          </View>
 
-                    <Image className="w-full h-[300px] mb-4 rounded-xl" source={{ uri: news[0].image_url}}/>
+          <Image
+            className="w-full h-[300px] mb-4 rounded-xl"
+            source={{ uri: news[0].image_url }}
+          />
 
-                    {news[0].content ? (
-                        <Text className="text-[14px] text-darkGrey leading-6">
-                            {news[0].content}
-                        </Text>
-                    ):(
-                        <Text className="text-[14px] text-darkGrey leading-6">
-                            {news[0].description}
-                        </Text>
-                    )}
-
-                </ScrollView>
-            )}
-            
-        </>
-    )
+          {news[0].content ? (
+            <Text className="text-[14px] text-darkGrey leading-6">
+              {news[0].content}
+            </Text>
+          ) : (
+            <Text className="text-[14px] text-darkGrey leading-6">
+              {news[0].description}
+            </Text>
+          )}
+        </ScrollView>
+      )}
+    </>
+  );
 }
