6e8fab86f9f85f643c28c3b27cee2d7979195b1a
Rana718 <ranadolui718@gmail.com>
2024-11-02 20:30:31 +0530

    update some styling in index page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,22 +1,38 @@
-import { Text, TextInput, View } from "react-native";
-import React from "react";
+import { TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Colors } from "@/constants/Colors";
 
 type Props = {
-    setSearchQuery: Function
+    setSearchQuery: Function,
 };
 
-export default function SearchBar({setSearchQuery}: Props) {
-    return(
+export default function SearchBar({ setSearchQuery }: Props) {
+    const [query, setQuery] = useState("");
+
+
+
+    return (
         <View className="px-3 pb-5">
             <View className="bg-gray-200 py-2 px-3 rounded-xl flex-row items-center">
-                <Ionicons name="search-outline" size={20} color={Colors.lightGrey}/>
-                <TextInput 
-                    onChangeText={(text) => setSearchQuery(text)}
-                    placeholder="Search" autoCapitalize="none" placeholderTextColor={Colors.lightGrey} 
+                { query.length === 0 &&
+                    <Ionicons name="search-outline" size={20} color={Colors.lightGrey} />
+                }
+                <TextInput
+                    onChangeText={(text) => {
+                        setQuery(text);
+                        setSearchQuery(text);
+                    }}
+                    value={query}
+                    placeholder="Search" autoCapitalize="none" placeholderTextColor={Colors.lightGrey}
                     className="text-black ml-2 text-lg flex-1"
                 />
+                {(query.length !== 0) && (
+                    <TouchableOpacity>
+                        <FontAwesome name="search" size={24} color={Colors.tint} />
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     )
